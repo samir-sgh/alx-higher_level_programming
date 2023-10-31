0ed6559dd1c7cb2082f0d0b51bb390bdfb714b09
MrSamSGH <samirsghiri@gmail.com>
2023-10-31 07:21:03 -0700

    update task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for c in range(ord('a'),ord('z')+1):
-    if (c != ord('q') and c != ord('e')):
-        print("{:c}".format(c), end='')
+for char in range(97, 123):
+    if (char != 101 and char != 113):
+        print("{:c}".format(char), end='')
